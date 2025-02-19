cf64a1867d1f9e8b1e03ce8fef429bf6abcd872d
AyparaKerimova <eelify1@gmail.com>
2025-02-19 19:29:43 +0400

    add get by id controller list

--- src/controllers/listController.js
+++ src/controllers/listController.js
@@ -1,14 +1,14 @@
-const List = require('../models/List');
+const List = require("../models/List");
 
 exports.addToList = async (req, res) => {
   try {
     const { movieId, serieId } = req.body;
     const userId = req.user.id;
-    
+
     if (!movieId && !serieId) {
-      return res.status(400).json({ message: 'Provide either a movieId or serieId' });
+      return res.status(400).json({ message: "Provide either a movieId or serieId" });
     }
-    
+
     const newItem = await List.create({ userId, movieId, serieId });
     res.status(201).json(newItem);
   } catch (error) {
@@ -19,8 +19,24 @@ exports.addToList = async (req, res) => {
 exports.getUserList = async (req, res) => {
   try {
     const userId = req.user.id;
-    const userList = await List.find({ userId }).populate('movieId serieId');
+    const userList = await List.find({ userId }).populate("movieId serieId");
     res.status(200).json(userList);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+exports.getListItemById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const userId = req.user.id;
+
+    const listItem = await List.findOne({ _id: id, userId }).populate("movieId serieId");
+    if (!listItem) {
+      return res.status(404).json({ message: "Item not found" });
+    }
+
+    res.status(200).json(listItem);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -28,16 +44,16 @@ exports.getUserList = async (req, res) => {
 
 exports.removeFromList = async (req, res) => {
   try {
-    const { id } = req.params; 
+    const { id } = req.params;
     const userId = req.user.id;
-    
+
     const listItem = await List.findOne({ _id: id, userId });
     if (!listItem) {
-      return res.status(404).json({ message: 'Item not found' });
+      return res.status(404).json({ message: "Item not found" });
     }
-    
+
     await List.findByIdAndDelete(id);
-    res.status(200).json({ message: 'Item removed' });
+    res.status(200).json({ message: "Item removed" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- src/routes/listRoutes.js
+++ src/routes/listRoutes.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const listController = require('../controllers/listController');
-const authMiddleware = require('../middleware/auth');
+const express = require("express");
+const listController = require("../controllers/listController");
+const authMiddleware = require("../middleware/auth");
 
 const router = express.Router();
 
 router.use(authMiddleware.protect);
-router.post('/add', listController.addToList);
-router.get('/', listController.getUserList);
-router.delete('/:id', listController.removeFromList);
+
+router.post("/add", listController.addToList);
+router.get("/:userId", listController.getUserList);
+router.get("/:id", listController.getListItemById); 
+router.delete("/remove/:id", listController.removeFromList);
 
 module.exports = router;
